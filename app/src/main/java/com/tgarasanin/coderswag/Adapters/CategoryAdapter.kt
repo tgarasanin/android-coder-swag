f78db837c7c7cf280098b9162f3fdd7f84c0d1ee
Teodora Garasanin <t.garasanin@levi9.com>
2020-04-06 13:34:11 +0200

    feat: add view holder

--- app/src/main/java/com/tgarasanin/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/tgarasanin/coderswag/Adapters/CategoryAdapter.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.tgarasanin.coderswag.Model.Category
 import com.tgarasanin.coderswag.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.category_list_item.view.*
 
 class CategoryAdapter (context: Context, categories: List<Category>): BaseAdapter() {
@@ -17,13 +19,26 @@ class CategoryAdapter (context: Context, categories: List<Category>): BaseAdapte
     val categories = categories
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var categoryView: View
-        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryView: View
+        val holderView: ViewHolder
+
+        if (convertView == null) {
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holderView = ViewHolder()
+            holderView.categoryImage = categoryView.imageView
+            holderView.categoryName = categoryView.textView
+
+            categoryView.tag = holderView
+        } else {
+            holderView = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
         val category = categories[position]
-        categoryView.textView.text = category.title
+        holderView.categoryName?.text = category.title
         val resourceId =
             context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryView.imageView.setImageResource(resourceId)
+        holderView.categoryImage?.setImageResource(resourceId)
         return categoryView
     }
 
@@ -38,5 +53,12 @@ class CategoryAdapter (context: Context, categories: List<Category>): BaseAdapte
     override fun getCount(): Int {
         return categories.count()
     }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+
+    }
+
 
 }
